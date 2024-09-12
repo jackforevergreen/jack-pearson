0a2bde31cf39a09de9cc65eadf38c2e1833d5471
jackforevergreen <jack@forevergreen.earth>
2024-09-12 16:32:15 -0400

    First Commit
    Creating the app adding GitHub Pages

---
+++ jack-pearson/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to My Personal Website</h1>
+      <p>
+        Hi there! I'm building this site to showcase my work and share my thoughts with the world.
+      </p>
+      <p>
+        Stay tuned for updates on my latest projects, blog posts, and more!
+      </p>
+    </div>
+  );
+}
+
+export default App;
